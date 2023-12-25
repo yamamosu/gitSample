8a37a0e651418af2faa3a185fb68a78c026f3b6b
yamamoto <yamamosu98@yahoo.co.jp>
2023-12-25 22:18:48 +0900

    conflict_test2

--- hello.java
+++ hello.java
@@ -3,5 +3,8 @@ class hello {
         System.out.println("hello world");
         System.out.println("Test Java");
         System.out.println("new branch");
+
+        // conflict_test
+        System.out.println("conflictBranch_conflict");
     }
 }
